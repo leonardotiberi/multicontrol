6840acc67dea85874f1d8884c4e8d7aeba8e32ac
leonardotiberi <leonardo.tiberi95@gmail.com>
2024-11-24 16:52:52 +0100

    config f2

--- climate.py
+++ climate.py
@@ -39,19 +39,23 @@ class MulticontrolClimate(CoordinatorEntity, ClimateEntity):  # noqa: D101
 
         self._attr_temperature_unit = "°C"
 
-        self._attr_target_temperature_high = 26 # cercare configurazione temp_setpoint_max
-        self._attr_target_temperature_low = 14 # cercare configurazione temp_setpoint_max
+        self._attr_target_temperature_high = (
+            26  # cercare configurazione temp_setpoint_max
+        )
+        self._attr_target_temperature_low = (
+            14  # cercare configurazione temp_setpoint_max
+        )
         self._attr_target_temperature_step = 0.1  # cercare configurazione temp_step
 
-        self._attr_max_temp = 26 # cercare configurazione temp_setpoint_max
+        self._attr_max_temp = 26  # cercare configurazione temp_setpoint_max
         self._attr_min_temp = 14  # cercare configurazione temp_setpoint_min
 
-        if node["config"]["fanSupport"]: # cercare fan_speed su config?
-            self._attr_supported_features = (ClimateEntityFeature.FAN_MODE | ClimateEntityFeature.TARGET_TEMPERATURE)
-        else:
-            self._attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE
+        # if node["config"]["fanSupport"]: # cercare fan_speed su config?
+        #     self._attr_supported_features = (ClimateEntityFeature.FAN_MODE | ClimateEntityFeature.TARGET_TEMPERATURE)
+        # else:
+        self._attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE
 
-        self.data = {} # unificare GetNodes e GetParams per ottenere subito i dati
+        self.data = {}  # unificare GetNodes e GetParams per ottenere subito i dati
 
     @property
     def available(self) -> bool:  # noqa: D102
