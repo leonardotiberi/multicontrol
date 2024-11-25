b5687eff9b5cb2d7b82af5fd6d85cf30838db8d8
Leonardo Tiberi <leonardo@sistema3.it>
2024-11-25 09:07:50 +0100

    add old code

---
+++ __init__.py
@@ -0,0 +1,211 @@
+from datetime import timedelta
+import logging
+
+from aiohttp import ClientSession
+
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.helpers import config_validation as cv
+import voluptuous as vol
+
+DOMAIN = "multicontrol"
+
+_LOGGER = logging.getLogger(DOMAIN)
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {vol.Required("username"): cv.string, vol.Required("password"): cv.string}
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+
+async def async_setup(hass, config):
+    hass.data["multicontrol"] = {"coordinator": MulticontrolCoordinator(hass, config)}
+    hass.helpers.discovery.load_platform("climate", DOMAIN, {}, config)
+    return True
+
+
+class MulticontrolCoordinator(DataUpdateCoordinator):
+    session: ClientSession
+
+    def __init__(self, hass, config):
+        super().__init__(
+            hass,
+            _LOGGER,
+            name="Zehnder Multicontrol",
+            update_interval=timedelta(seconds=5),
+        )
+        self.session = async_get_clientsession(hass)
+
+        c = config.get(DOMAIN)
+        if c is None:
+            _LOGGER.error("Configurazione non trovataper il dominio {DOMAIN}")
+
+        self.username = c["username"]
+        self.password = c["password"]
+        self.api_url = "https://api.rainmaker.espressif.com/v1/"
+        self.api_token = ""
+
+    async def _async_update_data(self):
+        return await self.getNodes()
+
+    async def login(self) -> None:
+        # LOGIN
+        _LOGGER.debug("Login")
+        async with self.session.post(
+            self.api_url + "login2",
+            json={"user_name": self.username, "password": self.password},
+        ) as response:
+            if response.status == 200:
+                res = await response.json()
+                self.api_token = res["accesstoken"]
+            else:
+                _LOGGER.error("Login fail")
+
+    async def getNodes(self) -> any:
+        await self.login()
+
+        # GET PARAMS
+        _LOGGER.debug("Get params nodes")
+        headers = {"Authorization": self.api_token}
+        async with self.session.get(
+            self.api_url + "user/nodes?node_details=1", headers=headers
+        ) as response:
+            if response.status == 200:
+                data = await response.json()
+            else:
+                _LOGGER.error("Ger params fail")
+                return False
+
+        nodes = {}
+        for node in data["node_details"]:
+            configs = {}
+            for config in node["config"]["devices"][0]["params"]:
+                configs[config["name"]] = config
+
+            params = node["params"]["multicontrol"]
+            nodes[node["id"]] = {
+                "id": node["id"],
+                "config": configs,
+                "connected": node["status"]["connectivity"]["connected"],
+                "name": params["Name"],
+                "alarm": params["alarm"],
+                "dehumidifier": params["dehumidifier"],
+                "humidity": params["humidity"],
+                "humidity_correction": params["humidity_correction"],
+                "post_treatment_automatic": params["post_treatment_automatic"],
+                "post_treatment_schedule": params["post_treatment_schedule"],
+                "radiant": params["radiant"],
+                "radiant_automatic": params["radiant_automatic"],
+                "radiant_enabled": params["radiant_enabled"],
+                "radiant_schedule": params["radiant_schedule"],
+                "season": params["season"],
+                "season_automatic": params["season_automatic"],
+                "temp": params["temp"],
+                "temp_setpoint": params["temp_setpoint"],
+                # fan params
+                "fan_speed": params.get("fan_speed"),
+            }
+        return nodes
+
+    async def setTargetTemperature(self, idx, value) -> None:
+        await self.login()
+
+        _LOGGER.debug("Set target temperature")
+        headers = {"Authorization": self.api_token}
+        async with self.session.put(
+            self.api_url + "user/nodes/params",
+            headers=headers,
+            json=[
+                {"node_id": idx, "payload": {"multicontrol": {"temp_setpoint": value}}}
+            ],
+        ) as response:
+            if response.status == 207:
+                _LOGGER.info("Target temperature ok")
+            else:
+                _LOGGER.error("Ger params fail")
+
+    async def setHeat(self, idx) -> None:
+        await self.login()
+
+        _LOGGER.debug("Set heat")
+        headers = {"Authorization": self.api_token}
+        async with self.session.put(
+            self.api_url + "user/nodes/params",
+            headers=headers,
+            json=[
+                {
+                    "node_id": idx,
+                    "payload": {"multicontrol": {"radiant_enabled": True}},
+                }  # set season
+            ],
+        ) as response:
+            if response.status == 207:
+                _LOGGER.info("Set heat ok")
+            else:
+                _LOGGER.error("Ger params fail")
+
+    async def setCool(self, idx) -> None:
+        await self.login()
+
+        _LOGGER.debug("Set heat")
+        headers = {"Authorization": self.api_token}
+        async with self.session.put(
+            self.api_url + "user/nodes/params",
+            headers=headers,
+            json=[
+                {
+                    "node_id": idx,
+                    "payload": {
+                        "multicontrol": {"radiant_enabled": False}
+                    },  # set season
+                }
+            ],
+        ) as response:
+            if response.status == 207:
+                _LOGGER.info("Set cool ok")
+            else:
+                _LOGGER.error("Ger params fail")
+
+    async def setOff(self, idx) -> None:
+        await self.login()
+
+        _LOGGER.debug("Set off")
+        headers = {"Authorization": self.api_token}
+        async with self.session.put(
+            self.api_url + "user/nodes/params",
+            headers=headers,
+            json=[
+                {
+                    "node_id": idx,
+                    "payload": {"multicontrol": {"radiant_enabled": False}},
+                }
+            ],
+        ) as response:
+            if response.status == 207:
+                _LOGGER.info("Set off ok")
+            else:
+                _LOGGER.error("Ger params fail")
+
+    async def setFanSpeed(self, idx, speed) -> None:
+        await self.login()
+
+        _LOGGER.debug("Set fan speed")
+        headers = {"Authorization": self.api_token}
+        async with self.session.put(
+            self.api_url + "user/nodes/params",
+            headers=headers,
+            json=[
+                {
+                    "node_id": idx,
+                    "payload": {"multicontrol": {"fan_speed": speed}},
+                }
+            ],
+        ) as response:
+            if response.status == 207:
+                _LOGGER.info("Set fan speed ok")
+            else:
+                _LOGGER.error("Ger params fail")
---
+++ climate.py
@@ -0,0 +1,147 @@
+from __future__ import annotations
+
+from homeassistant.components.climate import ClimateEntity
+from homeassistant.components.climate.const import ClimateEntityFeature, HVACMode
+from homeassistant.const import ATTR_TEMPERATURE
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+
+async def async_setup_platform(  # noqa: D103
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
+    if discovery_info is None:
+        return
+
+    nodes = await hass.data["multicontrol"]["coordinator"].getNodes()
+    for idx, node in nodes.items():
+        add_entities(
+            [MulticontrolClimate(hass.data["multicontrol"]["coordinator"], idx, node)]
+        )
+
+
+class MulticontrolClimate(CoordinatorEntity, ClimateEntity):  # noqa: D101
+    def __init__(self, coordinator, idx, node) -> None:  # noqa: D107
+        super().__init__(coordinator, context=idx)
+
+        self.idx = idx
+        self._available = False
+
+        fan_support = node["config"].get("fan_speed", None) is not None
+        if node["name"] == "Lavanderia":
+            print(fan_support)
+            print(node["config"])
+
+        self._attr_name = f"Termostato {node["name"]}"
+        self._attr_unique_id = f"multicontrol_climate_{idx}"
+
+        self._attr_hvac_modes = ["off", "cool", "heat"]
+
+        self._attr_temperature_unit = "°C"
+
+        self._attr_target_temperature_high = (
+            26  # cercare configurazione temp_setpoint_max
+        )
+        self._attr_target_temperature_low = (
+            14  # cercare configurazione temp_setpoint_max
+        )
+        self._attr_target_temperature_step = 0.1  # cercare configurazione temp_step
+
+        self._attr_max_temp = 26  # cercare configurazione temp_setpoint_max
+        self._attr_min_temp = 14  # cercare configurazione temp_setpoint_min
+
+        if fan_support:
+            self._attr_fan_modes = ("off", "low", "medium", "high")
+            self._attr_supported_features = (
+                ClimateEntityFeature.FAN_MODE | ClimateEntityFeature.TARGET_TEMPERATURE
+            )
+        else:
+            self._attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE
+
+        self.data = {}
+
+    @property
+    def available(self) -> bool:  # noqa: D102
+        return self.data.get("connected", False)
+
+    @property
+    def target_temperature(self) -> float | None:  # noqa: D102
+        return self.data.get("temp_setpoint", None)
+
+    @property
+    def current_temperature(self) -> float | None:  # noqa: D102
+        return self.data.get("temp", None)
+
+    @property
+    def target_humidity(self) -> float | None:  # noqa: D102
+        return self.data.get("humidity_correction", None)
+
+    @property
+    def current_humidity(self) -> float | None:  # noqa: D102
+        return self.data.get("humidity", None)
+
+    @property
+    def hvac_mode(self) -> HVACMode | None:  # noqa: D102
+        if self.data.get("radiant_enabled", False):
+            if self.data.get("season", None) == 1:
+                return HVACMode.HEAT
+            return HVACMode.COOL
+        return HVACMode.OFF
+
+    @property
+    def fan_mode(self):
+        speed = self.data.get("fan_speed", 0)
+        print(speed)
+        print(self.data)
+        if speed == 1:
+            return "low"
+        elif speed == 2:
+            return "medium"
+        elif speed == 3:
+            return "high"
+        return "off"
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        self.data = self.coordinator.data[self.idx]
+        self.async_write_ha_state()
+
+    async def async_set_hvac_mode(self, hvac_mode: HVACMode) -> None:  # noqa: D102
+        match hvac_mode:
+            case HVACMode.COOL:
+                await self.coordinator.setCool(self.idx)
+            case HVACMode.HEAT:
+                await self.coordinator.setHeat(self.idx)
+            case HVACMode.OFF:
+                await self.coordinator.setOff(self.idx)
+        await self.coordinator.async_request_refresh()
+
+    async def async_set_temperature(self, **kwargs) -> None:  # noqa: D102
+        if ATTR_TEMPERATURE not in kwargs:
+            raise ValueError("Temperatura non specificata")
+
+        target_temperature = kwargs[ATTR_TEMPERATURE]
+        await self.coordinator.setTargetTemperature(
+            self.idx,
+            target_temperature,
+        )
+        self._attr_target_temperature = target_temperature
+        await self.coordinator.async_request_refresh()
+
+    async def async_set_fan_mode(self, fan_mode: str) -> None:
+        mode = 0
+        match fan_mode:
+            case "low":
+                mode = 1
+            case "medium":
+                mode = 2
+            case "high":
+                mode = 3
+        await self.coordinator.setFanSpeed(self.idx, mode)
+        self._attr_fan_mode = fan_mode
+        await self.coordinator.async_request_refresh()
